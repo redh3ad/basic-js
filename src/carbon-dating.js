1bd8f5c6901639343f51a87923a1e80df4ff6750
Egor Matyas <79442740+redh3ad@users.noreply.github.com>
2021-03-13 01:18:32 +0300

    feat
    fixed bugs

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,3 +1,8 @@
+const CustomError = require("../extensions/custom-error");
+
+const MODERN_ACTIVITY= 15; 
+const HALF_LIFE_PERIOD= 5730;
+
 module.exports = function dateSample(sampleActivity) {
 
 if(!sampleActivity || typeof sampleActivity !== "string" || MODERN_ACTIVITY < sampleActivity || sampleActivity < 0){
