2238d76804effbf746518764021fd94d77aee85a
Egor Matyas <79442740+redh3ad@users.noreply.github.com>
2021-03-13 01:15:34 +0300

    feat
    changed code without mistakes

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,17 +1,14 @@
-const CustomError = require("../extensions/custom-error");
-
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
 module.exports = function dateSample(sampleActivity) {
 
-if (typeof(+sampleActivity) != Number) {
+if(!sampleActivity || typeof sampleActivity !== "string" || MODERN_ACTIVITY < sampleActivity || sampleActivity < 0){
 	return false;
 }
+let a = parseFloat(sampleActivity);
+  if(!a) {
+    return false;
+  }
 	let k = 0.693/HALF_LIFE_PERIOD;
-	let result = (Math.log(MODERN_ACTIVITY/sampleActivity))/k;
+	let result = (Math.log(MODERN_ACTIVITY/a))/k;
 	result = Math.ceil(result);
 	return result;
-
-};
-
+}
