44c99d4ba90f95b61005dd5e32b543e011b0926b
Egor Matyas <play.filin@gmail.com>
2022-03-02 14:43:24 +0300

    feat: add solution transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,56 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create transformed array based on the control sequences that original
- * array contains
- * 
- * @param {Array} arr initial array
- * @returns {Array} transformed array
- * 
- * @example
- * 
- * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
- * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new NotImplementedError(
+      `'arr' parameter must be an instance of the Array!`,
+    );
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+
+  let array = Array.from(arr);
+  array.forEach((elem, index) => {
+    switch (elem) {
+      case '--discard-next':
+        if (index + 1 >= array.length) {
+          array[index] = 'delete';
+          break;
+        }
+        array[index] = 'delete';
+        array[index + 1] = 'delete';
+        break;
+      case '--discard-prev':
+        if (index === 0) {
+          array[index] = 'delete';
+          break;
+        }
+        array[index] = 'delete';
+        array[index - 1] = 'delete';
+        break;
+      case '--double-next':
+        if (index + 1 >= array.length) {
+          array[index] = 'delete';
+          break;
+        }
+        array[index] = array[index + 1];
+        break;
+      case '--double-prev':
+        if (index == 0) {
+          array[index] = 'delete';
+          break;
+        }
+        array[index] = array[index - 1];
+        break;
+      default:
+        break;
+    }
+  });
+
+  return array.filter((elem) => elem !== 'delete');
 }
 
 module.exports = {
-  transform
+  transform,
 };
