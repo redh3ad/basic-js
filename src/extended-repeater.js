2177ba6146aa0e319d13e291f9988c3cf977df0e
Egor Matyas <play.filin@gmail.com>
2022-03-02 18:39:49 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,43 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Create a repeating string based on the given parameters
- *  
- * @param {String} str string to repeat
- * @param {Object} options options object 
- * @return {String} repeating string
- * 
- *
- * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
- *
- */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (str === undefined) {
+    throw Error();
+  }
+  if (options.addition === undefined) {
+    options.addition = '';
+  }
+  if (options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+  if (options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if (options.separator === undefined) {
+    options.separator = '+';
+  }
+  if (options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if (typeof str != 'string' || typeof options.addition != 'string') {
+    String(str);
+    String(options.addition);
+  }
+
+  str += String(options.addition);
+  for (let i = 1; i < Number(options.additionRepeatTimes); i++) {
+    str += options.additionSeparator + options.addition;
+  }
+
+  const replicateOfStr = str;
+
+  for (let i = 1; i < Number(options.repeatTimes); i++) {
+    str += options.separator + replicateOfStr;
+  }
+
+  return str;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
