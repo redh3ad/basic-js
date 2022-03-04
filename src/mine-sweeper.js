4ac11ae55e888aa042b36b88feb07c05ce99351e
Egor Matyas <play.filin@gmail.com>
2022-03-04 19:45:10 +0300

    feat: add solution mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,11 +23,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      if (i > 0) {
+        count += matrix[i - 1][j] ? 1 : 0;
+        if (j > 0) {
+          count += matrix[i - 1][j - 1] ? 1 : 0;
+        }
+        if (j < matrix[i - 1].length - 1) {
+          count += matrix[i - 1][j + 1] ? 1 : 0;
+        }
+      }
+
+      if (i < matrix.length - 1) {
+        count += matrix[i + 1][j] ? 1 : 0;
+        if (j > 0) {
+          count += matrix[i + 1][j - 1] ? 1 : 0;
+        }
+        if (j < matrix[i + 1].length - 1) {
+          count += matrix[i + 1][j + 1] ? 1 : 0;
+        }
+      }
+
+      if (j > 0) {
+        count += matrix[i][j - 1] ? 1 : 0;
+      }
+      if (j < matrix[i].length - 1) {
+        count += matrix[i][j + 1] ? 1 : 0;
+      }
+
+      result[i][j] = count;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
