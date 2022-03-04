d807ecee4f08fb4557548fed71c84a1a9bc5a81c
Egor Matyas <play.filin@gmail.com>
2022-03-04 16:23:27 +0300

    feat: add solution dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,11 +22,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let domainsObj = {};
+  let domainsArr = [...domains];
+  domainsArr
+    .map((domain) =>
+      domain
+        .split('.')
+        .reverse()
+        .map((domain) => '.' + domain),
+    )
+    .forEach((domain) => {
+      domain.reduce((sum, cur) => {
+        if (!domainsObj[sum + cur]) {
+          domainsObj[sum + cur] = 1;
+          return sum + cur;
+        } else {
+          domainsObj[sum + cur] += 1;
+          return sum + cur;
+        }
+      }, '');
+    });
+
+  return domainsObj;
+  // if (domains.length === 0) {
+  //   return domainsObj;
+  // }
+  // domainsArr.forEach((domain) => {
+  //   let tempDomain = domain.split('.').reverse().join('.');
+  //   tempDomain = '.' + tempDomain;
+
+  //   if (!domainsObj[tempDomain]) {
+  //     domainsObj[tempDomain] = 1;
+  //   }
+
+  //   let temp2 = tempDomain.split('.');
+  //   temp2.shift();
+  //   temp2.pop();
+
+  //   if (temp2.length === 1) {
+  //     temp2 = '.' + temp2[0];
+  //   } else {
+  //     temp2 = temp2.join('.');
+  //     temp2 = '.' + temp2;
+  //   }
+
+  //   if (!domainsObj[temp2]) {
+  //     domainsObj[temp2] = 1;
+  //   } else {
+  //     domainsObj[temp2] += 1;
+  //   }
+  // });
+  // return domainsObj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
